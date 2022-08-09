3f9c006bc81837993d0b3bdcab28e32820ddfb1f
Paul Yeung <paul.r.yeung@gmail.com>
2022-08-08 22:13:48 -0400

    added nav bar

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./Pages/HomePage/HomePage";
 
 import AboutPage from "./Pages/AboutPage/AboutPage";
+import NavBar from "./Components/NavBar/NavBar";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <NavBar />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 
 function NavBar(props) {
-  return <div className="NavBar"></div>;
+  return (
+    <div className="NavBar">
+      <Link to="/" className="nav-links">
+        Works
+      </Link>
+      <Link to="/about" className="nav-links">
+        About
+      </Link>
+    </div>
+  );
 }
 
 export default NavBar;

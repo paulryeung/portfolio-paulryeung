2587ab9b878af6f6bb62f71e05cc2ae3ce40005a
Paul Yeung <paul.r.yeung@gmail.com>
2022-08-13 13:00:24 -0400

    updated dynamic listing and add project seed file

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,59 +1,22 @@
 import React, { Component } from "react";
 import CardTile from "../../Components/CardTile/CardTile";
+const projects = require("../../projects.js");
 
 function HomePage(props) {
-  let projects = [
-    {
-      name: "Kirby Minesweeper",
-      description:
-        "Help Kirby clear bombs so he can resume his life of feasting.",
-      photo: "/images/kirby_square.jpg",
-      link: "https://paulryeung.github.io/Kirby-Minesweeper/",
-      button: "Help Kirby Now!",
-    },
-    {
-      name: "Avengers Initiative",
-      description:
-        "Tour the Avengers compound for top secret profiles and missions of your favorite heroes!",
-      photo: "/images/avengers.jpg",
-      link: "https://avengers-initiative.herokuapp.com/",
-      button: "Check It Out!",
-    },
-    {
-      name: "Garden-Smart",
-      description:
-        "Add and show off your garden of vegetables! Waste money on different inputs!",
-      photo: "/images/vegetables.jpg",
-      link: "",
-      button: "Start Planting!",
-    },
-  ];
-
   return (
     <div className="HomePage">
       <div className="Container">
         <h1>Welcome to Paul Yeung's Portfolio!</h1>
 
         <div className="cards-container">
-          <CardTile
-            name={projects[0].name}
-            description={projects[0].description}
-            photo={process.env.PUBLIC_URL + projects[0].photo}
-            link={projects[0].link}
-          />
-          <CardTile
-            name={projects[1].name}
-            description={projects[1].description}
-            photo={process.env.PUBLIC_URL + projects[1].photo}
-            link={projects[1].link}
-          />
-
-          <CardTile
-            name={projects[2].name}
-            description={projects[2].description}
-            photo={process.env.PUBLIC_URL + projects[2].photo}
-            link={projects[2].link}
-          />
+          {projects.map((project) => (
+            <CardTile
+              name={project.name}
+              description={projects.description}
+              photo={process.env.PUBLIC_URL + project.photo}
+              link={project.link}
+            />
+          ))}
         </div>
       </div>
     </div>
---
+++ src/projects.js
@@ -0,0 +1,44 @@
+let projects = [
+  {
+    name: "Kirby Minesweeper",
+    description:
+      "Help Kirby clear bombs so he can resume his life of feasting.",
+    photo: "/images/kirby_square.jpg",
+    link: "https://paulryeung.github.io/Kirby-Minesweeper/",
+    button: "Help Kirby Now!",
+  },
+  {
+    name: "Avengers Initiative",
+    description:
+      "Tour the Avengers compound for top secret profiles and missions of your favorite heroes!",
+    photo: "/images/avengers.jpg",
+    link: "https://avengers-initiative.herokuapp.com/",
+    button: "Check It Out!",
+  },
+  {
+    name: "Garden-Smart",
+    description:
+      "Add and show off your garden of vegetables! Waste money on different inputs!",
+    photo: "/images/vegetables.jpg",
+    link: "",
+    button: "Start Planting!",
+  },
+  {
+    name: "Kirby Minesweeper",
+    description:
+      "Help Kirby clear bombs so he can resume his life of feasting.",
+    photo: "/images/kirby_square.jpg",
+    link: "https://paulryeung.github.io/Kirby-Minesweeper/",
+    button: "Help Kirby Now!",
+  },
+  {
+    name: "Kirby Minesweeper",
+    description:
+      "Help Kirby clear bombs so he can resume his life of feasting.",
+    photo: "/images/kirby_square.jpg",
+    link: "https://paulryeung.github.io/Kirby-Minesweeper/",
+    button: "Help Kirby Now!",
+  },
+];
+
+module.exports = projects;

d14e282acf15d3a46ced67126372b7d0d9d37df4
Paul Yeung <paul.r.yeung@gmail.com>
2022-08-14 02:14:25 -0400

    heroku setup

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ class App extends Component {
             <Route path="/" element={<HomePage />} />
             <Route path="/about" element={<AboutPage />} />
             <Route path="/contact" element={<ContactPage />} />
+            <Route path="*" element={<HomePage />} />
           </Routes>
         </div>
       </div>

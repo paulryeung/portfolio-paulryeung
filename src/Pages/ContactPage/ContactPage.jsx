70a015c56ac733e0f3d5f8f1c5df8be11e2a3424
Paul Yeung <paul.r.yeung@gmail.com>
2022-08-20 13:36:01 -0400

    stable without nodemailer webpack problem

--- src/Pages/ContactPage/ContactPage.jsx
+++ src/Pages/ContactPage/ContactPage.jsx
@@ -1,6 +1,23 @@
 import React from "react";
+//const nodemailer = require("nodemailer");
 
 function ContactPage(props) {
+  // async function mail() {
+  //   let testAccount = await nodemailer.createTestAccount();
+
+  //   let transporter = nodemailer.createTransport({
+  //     host: "smtp.ethereal.email",
+  //     port: 587,
+  //     secure: false, // true for 465, false for other ports
+  //     auth: {
+  //       user: testAccount.user, // generated ethereal user
+  //       pass: testAccount.pass, // generated ethereal password
+  //     },
+  //   });
+
+  //   console.log("Code here for mailing");
+  // }
+
   return (
     <div className="ContactPage">
       <h1>Contact</h1>

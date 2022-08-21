e5b27cd8e4f18037a076dca09a61fe0ddc3651e9
Paul Yeung <paul.r.yeung@gmail.com>
2022-08-21 00:42:18 -0400

    started contact form

--- src/Pages/ContactPage/ContactPage.jsx
+++ src/Pages/ContactPage/ContactPage.jsx
@@ -1,26 +1,37 @@
 import React from "react";
-//const nodemailer = require("nodemailer");
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 function ContactPage(props) {
-  // async function mail() {
-  //   let testAccount = await nodemailer.createTestAccount();
-
-  //   let transporter = nodemailer.createTransport({
-  //     host: "smtp.ethereal.email",
-  //     port: 587,
-  //     secure: false, // true for 465, false for other ports
-  //     auth: {
-  //       user: testAccount.user, // generated ethereal user
-  //       pass: testAccount.pass, // generated ethereal password
-  //     },
-  //   });
-
-  //   console.log("Code here for mailing");
-  // }
-
   return (
     <div className="ContactPage">
-      <h1>Contact</h1>
+      <h1>Contact Me!</h1>
+      <div className="form-div">
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Name</Form.Label>
+            <Form.Control type="text" placeholder="Name" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Subject</Form.Label>
+            <Form.Control type="text" placeholder="Subject" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Message</Form.Label>
+            <Form.Control as="textarea" rows={4} placeholder="Hi Paul!" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
     </div>
   );
 }

06b46924768d363ed32197d25cb25a204a429b49
Paul Yeung <paul.r.yeung@gmail.com>
2022-08-23 09:30:18 -0400

    remove some clutter

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import WorksPage from "./Pages/WorksPage/WorksPage";
 import HomePage from "./Pages/HomePage/HomePage";
-import AboutPage from "./Pages/AboutPage/AboutPage";
+
 import ContactPage from "./Pages/ContactPage/ContactPage";
 import NavBar from "./Components/NavBar/NavBar";
 import "bootstrap/dist/css/bootstrap.min.css";
